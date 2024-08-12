493769606572c85770b49e05ccd91569d74c034d
OmarArturoGG <omararturomgg@gmail.com>
2024-08-11 22:14:02 -0600

    ejercicio2
    y video explicativo

---
+++ Ejercicio2/Ejercicio2/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Ejercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBienvenido = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.lblSaludo = new System.Windows.Forms.Label();
+            this.comboMenu = new System.Windows.Forms.ComboBox();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBienvenido
+            // 
+            this.lblBienvenido.AutoSize = true;
+            this.lblBienvenido.Location = new System.Drawing.Point(227, 32);
+            this.lblBienvenido.Name = "lblBienvenido";
+            this.lblBienvenido.Size = new System.Drawing.Size(293, 16);
+            this.lblBienvenido.TabIndex = 0;
+            this.lblBienvenido.Text = "Bienvenido, ingrese un numero de cuenta valido";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(230, 70);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(290, 22);
+            this.txtNumero.TabIndex = 1;
+            this.txtNumero.TextChanged += new System.EventHandler(this.txtNumero_TextChanged);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(293, 115);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(174, 23);
+            this.btnIniciar.TabIndex = 2;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // lblSaludo
+            // 
+            this.lblSaludo.AutoSize = true;
+            this.lblSaludo.Location = new System.Drawing.Point(227, 170);
+            this.lblSaludo.Name = "lblSaludo";
+            this.lblSaludo.Size = new System.Drawing.Size(0, 16);
+            this.lblSaludo.TabIndex = 3;
+            // 
+            // comboMenu
+            // 
+            this.comboMenu.FormattingEnabled = true;
+            this.comboMenu.Location = new System.Drawing.Point(230, 226);
+            this.comboMenu.Name = "comboMenu";
+            this.comboMenu.Size = new System.Drawing.Size(290, 24);
+            this.comboMenu.TabIndex = 4;
+            this.comboMenu.Text = "Menu";
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(293, 310);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(174, 23);
+            this.btnEjecutar.TabIndex = 5;
+            this.btnEjecutar.Text = "Siguiente";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(227, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 6;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(293, 365);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(174, 23);
+            this.btnCerrar.TabIndex = 7;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.comboMenu);
+            this.Controls.Add(this.lblSaludo);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblBienvenido);
+            this.Name = "Form1";
+            this.Text = "Cajero Electrónico";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBienvenido;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label lblSaludo;
+        private System.Windows.Forms.ComboBox comboMenu;
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
+
---
+++ Ejercicio2/Ejercicio2/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2
+{
+    public partial class Form1 : Form
+    {
+
+        private int[] cuentas = { 1000001, 1000002, 1000004 };
+        private double[] saldos = { 400, 200, 500 };
+        private int cuentaActual = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            comboMenu.Items.Add("Consulta de saldo");
+            comboMenu.Items.Add("Retiro de fondos");
+            comboMenu.Items.Add("Consignaciones");
+            comboMenu.Items.Add("Transferencias");
+            comboMenu.Items.Add("Salir");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            int cuentaIngresada;
+
+            try
+            {
+                cuentaIngresada = Convert.ToInt32(txtNumero.Text);
+
+                int indiceCuenta = Array.IndexOf(cuentas, cuentaIngresada);
+
+                if (indiceCuenta != -1)
+                {
+                    cuentaActual = indiceCuenta;
+                    lblSaludo.Text = "Bienvenido, su número de cuenta es " + cuentaIngresada;
+                    comboMenu.Enabled = true;
+                    btnEjecutar.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Número de cuenta no válido, vuelva a intentarlo");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Número de cuenta no válido, vuelva a intentarlo");
+            }
+        }
+
+
+    private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            if(cuentaActual == -1)
+            {
+                MessageBox.Show("Ingrese un numero de cuenta primero");
+                return;
+            }
+
+            string opcionSeleccionada = comboMenu.SelectedItem.ToString();
+            switch (opcionSeleccionada)
+            {
+                case "Consulta de saldo":
+                    MessageBox.Show("El saldo actual de cuenta " + cuentas[cuentaActual]+" es de $"+saldos[cuentaActual].ToString("0.00"));
+                    break;
+
+                case "Retiro de fondos":
+                    double cantidadRetiro;
+                    if(double.TryParse(InputBox("Ingrese la cantidad a retirar: "),out cantidadRetiro))
+                    {
+                        if (cantidadRetiro > saldos[cuentaActual])
+                        {
+                            MessageBox.Show("Saldo insuficiente");
+                        }
+                        else
+                        {
+                            saldos[cuentaActual] -= cantidadRetiro;
+                            MessageBox.Show("Retiro exitoso. El saldo actual de la cuenta " + cuentas[cuentaActual] + " es de $" + saldos[cuentaActual].ToString("0.00"));
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cantidad no valida");
+                    }
+                    break;
+
+                case "Consignaciones":
+                    double cantidadConsignacion;
+                    if(double.TryParse(InputBox("Ingrese la cantidad a consignar: "),out cantidadConsignacion))
+                    {
+                        saldos[cuentaActual] += cantidadConsignacion;
+                        MessageBox.Show("Consignación exitosa. El saldo actual de la cuenta " + cuentas[cuentaActual] + " es de $" + saldos[cuentaActual].ToString("0.00"));
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("Cantidad no válida");
+                    }
+                    break;
+
+                case "Transferencias":
+                    int cuentaDestino;
+                    if (int.TryParse(InputBox("Ingrese cuenta destino:"), out cuentaDestino))
+                    {
+                        int cuentaDestino2 = Array.IndexOf(cuentas, cuentaDestino);
+                        if (cuentaDestino2 != -1)
+                        {
+                            double cantidadTransferencia;
+                            if (double.TryParse(InputBox("Ingrese la cantidad a transferir:"), out cantidadTransferencia))
+                            {
+                                if(cantidadTransferencia > saldos[cuentaActual])
+                                {
+                                    MessageBox.Show("Saldo insuficiente");
+                                }
+                                else
+                                {
+                                    saldos[cuentaActual] -= cantidadTransferencia;
+                                    saldos[cuentaDestino2] += cantidadTransferencia;
+                                    MessageBox.Show("Transferencia exitosa. El saldo actual de la cuenta " + cuentas[cuentaActual] + " es de $" + saldos[cuentaActual].ToString("0.00"));
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Cantidad no válida");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("numero de destino no valido");
+                        }
+                      
+                     }break;
+
+                        case "Salir":
+                            Application.Exit();
+                            break;
+
+                        default:
+                            MessageBox.Show("Opcion no valida");
+                    break;
+             }
+        }
+        private string InputBox(string prompt)
+        {
+
+            using (Form form = new Form())
+            {
+                form.Text = prompt;
+                form.Width = 300;
+                form.Height = 150;
+                form.StartPosition = FormStartPosition.CenterScreen;
+
+                Label label = new Label { Left = 20, Top = 20, Text = prompt };
+                form.Controls.Add(label);
+
+                TextBox textBox = new TextBox { Left = 20, Top = 50, Width = 250 };
+                form.Controls.Add(textBox);
+
+                Button button = new Button { Text = "OK", Left = 200, Top = 70, DialogResult = DialogResult.OK };
+                form.Controls.Add(button);
+                form.AcceptButton = button;
+
+                DialogResult result = form.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    return textBox.Text;
+                }
+                else
+                {
+                    return "";
+                }
+            }
+        }
+
+
+        private void txtNumero_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
