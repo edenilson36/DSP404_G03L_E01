0e3275261ce4ff575e8cc9851badf29532af1fe4
Jose Manuel Reyes Echeveria <jose.eche777@gmail.com>
2024-09-23 16:19:43 -0600

    Guia_07

---
+++ RE242138_guia_07/Guia7/CajeroWeb/Controllers/NacionalesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CajeroWeb.Models;
+
+namespace CajeroWeb.Controllers
+{
+    public class NacionalesController : Controller
+    {
+        // GET: Nacionales
+        public ActionResult Nacionales(string sNumeroTarjeta, double sSaldo)
+        {
+            ViewBag.sNumeroTarjeta = sNumeroTarjeta;
+            ViewBag.sSaldo = sSaldo;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nacionales(string sNumeroTarjeta, double sSaldo, double cantidad)
+        {
+            double Saldo;
+            //Se crea una instancia de la clase transacción y se le envían dos parámetros
+            transaccion nuevaTransaccion = new transaccion(sNumeroTarjeta, sSaldo);
+
+            if (cantidad > 0)
+            {
+                nuevaTransaccion.transferenciaNacional(cantidad);
+
+            }
+            sSaldo = nuevaTransaccion.getSaldo();
+            ViewBag.sNumeroTarjeta = nuevaTransaccion.getNumTarjeta();
+            ViewBag.sSaldo = nuevaTransaccion.getSaldo();
+            return Redirect("~/Menu/Menu?sNumeroTarjeta=" + sNumeroTarjeta + "&sSaldo=" + sSaldo);
+
+        }
+
+
+
+
+    }
+}
